8734b00bdf62daef6210908a59f5c4f33471868a
IlyasWaseem <ilyas.prime@gmail.com>
2021-12-14 13:17:08 +0530

    initial setup

---
+++ src/main/java/com/emmigration/systemdetails/SystemdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.emmigration.systemdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemdetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmigration/systemdetails/SystemdetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmigration.systemdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
